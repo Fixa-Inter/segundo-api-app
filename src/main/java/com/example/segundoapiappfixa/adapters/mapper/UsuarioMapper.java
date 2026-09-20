dcd9cc34fab48929a4f26e9034972f09ce989268
ryancursinom <rcursinomoraes@gmail.com>
2026-09-20 19:49:51 -0300

    feat: endpoint de acesso aos dados de perfil do usuário logado

--- src/main/java/com/example/segundoapiappfixa/adapters/mapper/UsuarioMapper.java
+++ src/main/java/com/example/segundoapiappfixa/adapters/mapper/UsuarioMapper.java
@@ -1,13 +1,19 @@
 package com.example.segundoapiappfixa.adapters.mapper;
 
+import com.example.segundoapiappfixa.adapters.dto.output.Usuario.UsuarioOutputDTO;
 import com.example.segundoapiappfixa.domain.model.Usuario;
 import com.example.segundoapiappfixa.infrastructure.database.entity.UsuarioEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
     Usuario toModel(UsuarioEntity entity);
     UsuarioEntity toEntity(Usuario model);
+
+    @Mapping(source = "tipoAcesso.nome", target = "tipoAcesso")
+    @Mapping(source = "endereco.cnpj", target = "cnpjEndereco")
+    UsuarioOutputDTO toOutputDTO(Usuario usuario);
 
 }
